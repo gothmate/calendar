95b568cc6e0c95bcc34ed2ee0c5cdf9fcbb40e00
gothmate <gothmate@gmail.com>
2025-02-23 22:29:38 -0300

    primeiro commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from '@/app/page.module.sass'
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h5>Contato</h5>
+    </footer>
+  )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import styles from '@/app/page.module.sass'
+export function Header() {
+  return (
+		<header className={styles.header}>
+			<h2>Agenda Eletrônica</h2>
+		</header>
+	);
+}
